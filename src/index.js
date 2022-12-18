23ed34c89085a6a400082055b1d8a848da3d7853
abdiwoli <abdiwolih@gmail.com>
2022-12-18 17:01:21 -0600

    update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-var hW1 = React.createElement("h1", null, "hello abdi");
-var hW2 = React.createElement("h2", null, "hello abdi");
-
-var div1 = Reac.createElement("div",null, hW1, hW2);
-
-ReactDom.render(
-  div1,
+var h1 = React.createElement('h1',null, 'abdiwoli')
+var h2 = React.createElement('h1', null, 'abdiwoli')
+var div1 = React.createElement('div', null, h1,h2);
+ReactDOM.render(
+  h1,
   document.getElementById('app')
 )
